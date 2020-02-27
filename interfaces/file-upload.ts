2c9c16e95a99f071cfe80bc467fdec965f81fe71
JUANDATM <kira00711@hotmail.com>
2020-02-27 16:05:20 -0600

    Uploads y rutas Ok

---
+++ interfaces/file-upload.ts
@@ -0,0 +1,10 @@
+export interface FileUpload{
+    name: string;
+    data: any;
+    size: String,
+    encoding: String ,
+    tempFilePath: String ,
+    truncated: boolean ,
+    mimetype: String ,
+    md5: string
+}
